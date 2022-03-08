ac2e80704c15ce9be5efc0ea880e931cb605f1df
mohamedmousa153 <mohamedamousa153@gmail.com>
2022-03-08 11:19:51 +0200

    .

---
+++ lib/screens/registration/registration.dart
@@ -0,0 +1,73 @@
+import 'package:find_doctor/screens/signin/signin.dart';
+import 'package:find_doctor/screens/signup/signup.dart';
+import 'package:flutter/material.dart';
+
+import '../../shared/app_button.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                clipBehavior: Clip.antiAlias,
+                child: Image(
+                  image: AssetImage('assets/images/teriaq.PNG'),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(80),
+                  color: Colors.white,
+                ),
+                height: 150,
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              Text(
+                "Welacome To Teriag!",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              AppButton(
+                "Sign Up",
+                bottenColor: Colors.blue,
+                textColor: Colors.white,
+                borderradius: BorderRadius.circular(60),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUp()),
+                  );
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              AppButton(
+                "Sign Up",
+                bottenColor: Colors.white,
+                textColor: Colors.blue,
+                borderradius: BorderRadius.circular(60),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignIn()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:find_doctor/screens/welcome/welcomescreen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      Timer(
+        const Duration(seconds: 3),
+        (() {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => WelcomeScreen()),
+          );
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        child: Image.asset(
+          'assets/images/heartbeat.gif',
+          height: 150.h,
+        ),
+      ),
+    );
+  }
+}
